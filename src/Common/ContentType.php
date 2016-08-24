4111ddbaf3e4a283837f9d01923f3a40b9491542
Martin Brecht-Precht <mb@markenwerk.net>
2016-08-24 15:12:24 +0200

    Added the correct ContentType class constants

--- src/Common/ContentType.php
+++ src/Common/ContentType.php
@@ -11,13 +11,11 @@ final class ContentType
 {
 
 	const TYPE_CODE = 'CODE';
-	const TYPE_DIVISION = 'DIVISION';
-	const TYPE_HEADING = 'HEADING';
-	const TYPE_UNORDERED_LIST = 'UNORDERED_LIST';
-	const TYPE_ORDERED_LIST = 'ORDERED_LIST';
-	const TYPE_LIST_ITEM = 'LIST_ITEM';
-	const TYPE_PARAGRAPH = 'PARAGRAPH';
-	const TYPE_QUOTE = 'QUOTE';
+	const TYPE_EMPHASIS = 'EMPHASIS';
+	const TYPE_IMAGE = 'IMAGE';
+	const TYPE_LINE_BREAK = 'LINE_BREAK';
+	const TYPE_LINK = 'LINK';
+	const TYPE_TEXT = 'TEXT';
 	const TYPE_HTML = 'HTML';
 
 }
