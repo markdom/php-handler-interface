b059dfb093aff42ea11593fa55489530eed5b3c4
Martin Brecht-Precht <mb@markenwerk.net>
2016-08-30 12:24:10 +0200

    Added pseudo enum classes.

---
+++ src/Common/EmphasisLevel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Markdom\Common;
+
+/**
+ * Class EmphasisLevel
+ *
+ * @package Markdom\Common
+ */
+class EmphasisLevel
+{
+
+	const LEVEL_1 = 'LEVEL_1';
+	const LEVEL_2 = 'LEVEL_2';
+
+}
---
+++ src/Common/HeadingLevel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Markdom\Common;
+
+/**
+ * Class EmphasisLevel
+ *
+ * @package Markdom\Common
+ */
+class EmphasisLevel
+{
+
+	const LEVEL_1 = 'LEVEL_1';
+	const LEVEL_2 = 'LEVEL_2';
+	const LEVEL_3 = 'LEVEL_3';
+	const LEVEL_4 = 'LEVEL_4';
+	const LEVEL_5 = 'LEVEL_5';
+	const LEVEL_6 = 'LEVEL_6';
+
+}
